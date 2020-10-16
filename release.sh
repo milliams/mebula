268147bbd72640c8c993b7014f0605de9606f952
Matt Williams <matt@milliams.com>
2020-10-16 10:09:18 +0100

    Update release script based on nbpretty

--- release.sh
+++ release.sh
@@ -29,6 +29,12 @@ if ! git diff --cached --quiet
     exit 1
 fi
 
+if ! poetry build -q
+  then
+    echo "Poetry build failed:"
+    poetry build
+fi
+
 echo "Are you sure you want to release the next ${version_spec} version?"
 select yn in "Yes" "No"; do
     case $yn in
@@ -40,9 +46,8 @@ done
 # Bump version in pyproject.toml
 poetry version "${version_spec}"
 new_version=$(poetry version | awk '{print $2}')
-
 git add pyproject.toml
+
 git commit -m "Update to version ${new_version}"
 git tag "${new_version}"
-git push --all
-git push --tags
+git push --atomic --tags origin HEAD
